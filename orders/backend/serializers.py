ee72034af33ca901cca7fed480962f6fb4310e1b
igare4eg <sokolik1063@mail.ru>
2023-11-09 00:24:41 -0300

    commit 09.11

--- orders/backend/serializers.py
+++ orders/backend/serializers.py
@@ -1,30 +1,92 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-from backend.models import Shop, Product
+from backend.models import User, Category, Shop, ProductInfo, Product, ProductParameter, OrderItem, Order, Contact
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    class Meta:
+        model = Contact
+        fields = ('id', 'city', 'street', 'house', 'structure', 'building', 'apartment', 'user', 'phone')
+        read_only_fields = ('id',)
+        extra_kwargs = {
+            'user': {'write_only': True}
+        }
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name',)
+        read_only_fields = ('id',)
 
 
 class ShopSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shop
-        fields = ('id', 'name', 'state')
+        fields = ('id', 'name', 'state',)
+        read_only_fields = ('id',)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    contacts = ContactSerializer(read_only=True, many=True)
+    shops = ShopSerializer(read_only=True, many=True)
+    categories = CategorySerializer(read_only=True, many=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name', 'email', 'company', 'position', 'contacts')
+        read_only_fields = ('id',)
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    shop = ShopSerializer(read_only=True)
+    category = serializers.StringRelatedField()
 
     class Meta:
         model = Product
-        fields = ('id', 'name', 'category', 'quantity', 'price', 'shop')
+        fields = ('name', 'category',)
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class ProductParameterSerializer(serializers.ModelSerializer):
+    parameter = serializers.StringRelatedField()
+
     class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
+        model = ProductParameter
+        fields = ('parameter', 'value',)
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class ProductInfoSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    product_parameters = ProductParameterSerializer(read_only=True, many=True)
+
     class Meta:
-        model = Group
-        fields = ['url', 'name']
+        model = ProductInfo
+        fields = ('id', 'model', 'product', 'shop', 'quantity', 'price', 'price_rrc', 'product_parameters',)
+        read_only_fields = ('id',)
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ('id', 'product_info', 'quantity', 'order',)
+        read_only_fields = ('id',)
+        extra_kwargs = {
+            'order': {'write_only': True}
+        }
+
+
+class OrderItemCreateSerializer(OrderItemSerializer):
+    product_info = ProductInfoSerializer(read_only=True)
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    ordered_items = OrderItemCreateSerializer(read_only=True, many=True)
+
+    total_sum = serializers.IntegerField()
+    contact = ContactSerializer(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ('id', 'ordered_items', 'state', 'dt', 'total_sum', 'contact',)
+        read_only_fields = ('id',)
--- orders/orders/urls.py
+++ orders/orders/urls.py
@@ -16,20 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-
-from backend.views import ProductView, UserViewSet, GroupViewSet, import_data
-
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', ProductView.as_view()),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('import/', import_data, name='import_data')
+    path('api/v1/', include('backend.urls', namespace='backend'))
 ]
-
-urlpatterns += router.urls
